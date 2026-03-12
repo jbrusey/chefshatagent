bdd7f79b25bf85a93eef541ae19520ad501692c2
James Brusey <james.brusey@gmail.com>
2026-03-12 15:56:17 +0000

    Fix IrpsAdapter mask method keyword compatibility

--- game_adapters.py
+++ game_adapters.py
@@ -115,7 +115,7 @@ class IrpsAdapter(GameAdapter):
 
         return bool(info.get("win", False))
 
-    def extract_valid_action_mask(self, _obs: Any, action_space_n: int) -> np.ndarray:
+    def extract_valid_action_mask(self, obs: Any, action_space_n: int) -> np.ndarray:
         return np.ones(action_space_n, dtype=bool)
 
     def format_env_action(self, policy_action: int, action_space_n: int) -> int:
